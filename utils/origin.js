465c182047d67ac8a027b1a3b341e78fa83624b8
adehenry <adefemiadelaja@gmail.com>
2023-07-03 11:41:58 +0100

    modified deployment settings

--- utils/origin.js
+++ utils/origin.js
@@ -1,6 +1,6 @@
 let origin =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000/"
-    : "https://shopi-new.vercel.app/";
+    : "https://shopi-ten.vercel.app/";
 
 export default origin;
