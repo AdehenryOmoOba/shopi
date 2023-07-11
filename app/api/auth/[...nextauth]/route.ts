4ab6bb11ef862c94fcbc45f045dc8de57665debd
adehenry <adefemiadelaja@gmail.com>
2023-07-11 16:27:08 +0100

    fixed social login bug

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,22 @@
 import NextAuth from "next-auth/next"
-import { nextAuthoptions } from "./nextAuthOptions"
+import { NextAuthOptions } from "next-auth"
+import GithubProvider from "next-auth/providers/github";
+import origin from "@/utils/origin";
 
+
+
+export const nextAuthoptions: NextAuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        }),
+    ],
+    pages: {
+        signIn: `${origin}login`
+    },
+    secret: "KWEVPOM3M3F33F33PMFP",
+}
 
 const handler = NextAuth(nextAuthoptions)
 
